5f573aa2c71f194bf68a77bdb3ce1325eb0e3836
Gopala Krishna Koduri <gopala.koduri@gmail.com>
2014-04-05 20:00:11 +0200

    Many changes to the code, and data for Ambati

---
+++ src/text_processor.py
@@ -0,0 +1,42 @@
+import pickle
+import codecs
+import requests
+from os.path import exists, basename
+
+
+from . import nerpari
+from mycelery import app
+
+#Data
+#wiki_index = pickle.load(file(expanduser('~')+'/workspace/relation-extraction/data/wiki_index.pickle'))
+wiki_index = {}
+
+
+@app.task
+def plain_text(page, f_path):
+    """
+    Gets the plain text, cleans them
+    """
+    if exists(f_path):
+        print False
+    p = nerpari.Page(page)
+    p.set_content(wiki_index)
+    try:
+        p.clean_content()
+    except:
+        print False
+    try:
+        p.serialize_content(f_path)
+    except:
+        print False
+    print True
+
+
+@app.task
+def entity_link(input_file, out_dir, spotlight_server):
+    text = codecs.open(input_file).read()
+    res = requests.post('http://'+spotlight_server+'.s.upf.edu:2222/rest/annotate',
+                        data={'text': text, 'confidence': '0.2', 'support': '5'},
+                        headers={"Accept": "application/json"})
+    output_file = out_dir+basename(input_file)[:-4]+'.pickle'
+    pickle.dump(res.json(), file(output_file, 'w'))
